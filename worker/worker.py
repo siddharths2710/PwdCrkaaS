4744e97877544cd7c335aac01c9956d856719b94
Siddharth Srinivasan <siddharths2710@yahoo.com>
2022-11-20 10:58:06 -0500

    Added backend prototype code

--- worker/worker.py
+++ worker/worker.py
@@ -1,5 +1,3 @@
-print("Worker Init")
-
 import os
 import sys
 import glob
@@ -11,7 +9,9 @@ import platform
 import tempfile
 import subprocess
 import minio.error
+import pandas as pd
 from enum import Enum
+import sqlalchemy as db
 
 __author__ = "Siddharth Srinivasan"
 
@@ -24,13 +24,17 @@ __author__ = "Siddharth Srinivasan"
 redisHost = os.getenv("REDIS_HOST") or "localhost"
 redisPort = int(os.getenv("REDIS_PORT") or "6379")
 
+WORK_KEY, TERMINATE_KEY = "toWorker", 'toTerminate'
 INPUT_BUCKET_NAME = os.getenv("INPUT_BUCKET_NAME") or 'lab7-queue'
 OUTPUT_BUCKET_NAME = os.getenv("OUTPUT_BUCKET_NAME") or 'lab7-output'
+JOHN_OUTPUT_FILE = os.path.join(os.path.expanduser('~'), '.john/john.pot')
+
 
 class LOGLEVEL(Enum):
     INFO = 0
     DEBUG = 1
     ERROR = 2
+
 
 redisHost, redisPort = os.getenv("REDIS_HOST") or "localhost", int(os.getenv("REDIS_PORT") or "6379")
 minioHost, minioPort = os.getenv("MINIO_HOST") or "localhost", int(os.getenv("MINIO_PORT") or "9000")
@@ -43,6 +47,62 @@ redisClient = redis.StrictRedis(host=redisHost, port=redisPort, db=0)
 minioClient = minio.Minio(f"{minioHost}:{minioPort}",
                access_key=minioUser,
                secret_key=minioPwd, secure=False)
+
+mysql_db = os.getenv('MYSQL_DB') or 'foo'
+mysql_uri = os.getenv('MYSQL_URI') or 'localhost'
+mysql_user = os.getenv('MYSQL_USER') or 'scott'
+mysql_pwd = os.getenv('MYSQL_PWD') or 'tiger'
+db_engine = db.create_engine(f"mysql://{mysql_user}:{mysql_pwd}@{mysql_uri}/{mysql_db}")
+db_connection = db_engine.connect()
+
+# https://stackoverflow.com/questions/33053241/sqlalchemy-if-table-does-not-exist
+
+
+# https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_creating_table.htm
+# https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91
+meta = db.MetaData()
+Password_Outputs = db.Table(
+    'password_outputs', meta,
+    db.Column('userId', db.Integer, primary_key=True, nullable=False),
+    db.Column('hash', db.String, primary_key=True, nullable=False),
+    db.Column('password', db.String, nullable=False),
+    db.Column('hash_type', db.String, primary_key=True, nullable=False),
+    db.Column('salt', db.String, nullable=False)
+)
+
+if not db_engine.dialect.has_table(db_connection, 'password_outputs'): meta.create_all(db_engine)
+
+id_hash_map = {}
+id_process_map = {}
+
+def _get_user_id(pwd_hash):
+    for user_id in id_hash_map:
+        if pwd_hash in id_hash_map[user_id]:
+            return user_id
+    return None
+
+def _handle_termination(terminate_id):
+    if terminate_id not in id_process_map: redisClient.lpush(TERMINATE_KEY, terminate_id); return False
+    prc = id_process_map[terminate_id]
+    prc.terminate()
+    id_process_map.pop(terminate_id)
+    return True
+
+def _publish_password_outputs():
+    try:
+        pwd_file = open(JOHN_OUTPUT_FILE, 'r')
+        for line in pwd_file:
+            hash, pwd = line.split(':')
+            user_id = _get_user_id(hash)
+            if user_id is None:
+                pwd_file.close()
+                raise Exception("User id not found for hash: " + hash)
+            
+        pwd_file.close()
+    except FileNotFoundError as e:
+        log_output("_publish_password_outputs: Pwd output file not created for flushing to db", log_level=LOGLEVEL.ERROR)
+    except Exception as e:
+        log_output("_publish_password_outputs: %s" % repr(e), log_level=LOGLEVEL.ERROR)
 
 def log_output(message, key=logKey, log_level=log_level):
     print("Worker", log_level.name, ":", message, file=sys.stdout)
@@ -61,7 +121,15 @@ except minio.error.InvalidResponseError as e: log_output("MinIO API Error: " + r
 except Exception as e: log_output("Error: " + repr(e), log_level=LOGLEVEL.ERROR)
 
 while True:
-        work = redisClient.blpop("toWorker", timeout=0)
+        is_terminated, is_completed = False, False
+        terminate_id = redisClient.lpop(TERMINATE_KEY, timeout=0)
+        if terminate_id is not None: is_terminated = _handle_termination(terminate_id); continue
+        for user_id in id_process_map:
+            if id_process_map[user_id].poll() == 0: is_completed = True; break
+        if is_terminated or is_completed: _publish_password_outputs()
+                
+        
+        work = redisClient.lpop(WORK_KEY, timeout=0)
         mp3_hash = work[1].decode('utf-8')
         log_output(f"Received {mp3_hash} for separation")
         try:
